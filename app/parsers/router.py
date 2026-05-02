fa4c6841e7d617284a041b0963ea257d56b0010b
maisatyamhoon <satyam.saurav2022@vitstudent.ac.in>
2026-05-03 04:40:24 +0530

    Fix parser router to use detect_provider

--- app/parsers/router.py
+++ app/parsers/router.py
@@ -1,20 +1,19 @@
-from app.parsers.detector import detect_parser
+from app.parsers.detector import detect_provider
+from app.parsers.extract import extract_pdf_text
 from app.parsers.gpay import parse_gpay
 from app.parsers.paytm import parse_paytm
 from app.parsers.phonepe import parse_phonepe
 
 
 def parse_file(file_bytes: bytes, password: str = None):
-    text = detect_parser(file_bytes, password=password)
+    provider = detect_provider(file_bytes, password=password)
+    raw_text = extract_pdf_text(file_bytes)
 
-    parser = text["parser"]
-    raw_text = text["text"]
-
-    if parser == "gpay":
+    if provider == "gpay":
         return parse_gpay(raw_text)
-    elif parser == "paytm":
+    elif provider == "paytm":
         return parse_paytm(raw_text)
-    elif parser == "phonepe":
+    elif provider == "phonepe":
         return parse_phonepe(raw_text)
 
     return []
