005d6b9c6ee954a03f87bf9cf52a85d885eaf3f9
maisatyamhoon <satyam.saurav2022@vitstudent.ac.in>
2026-05-03 04:28:57 +0530

    Make parser router accept file bytes

--- app/parsers/router.py
+++ app/parsers/router.py
@@ -1,19 +1,20 @@
 from app.parsers.detector import detect_parser
-from app.parsers.paytm import parse_paytm_pdf
-from app.parsers.gpay import parse_gpay_pdf
+from app.parsers.gpay import parse_gpay
+from app.parsers.paytm import parse_paytm
+from app.parsers.phonepe import parse_phonepe
 
 
-def parse_file(file):
-    parser = detect_parser(file)
-    print("DETECTED PARSER:", parser)
+def parse_file(file_bytes: bytes, password: str = None):
+    text = detect_parser(file_bytes, password=password)
 
-    file.file.seek(0)
+    parser = text["parser"]
+    raw_text = text["text"]
 
-    if parser == "paytm":
-        return parse_paytm_pdf(file)
+    if parser == "gpay":
+        return parse_gpay(raw_text)
+    elif parser == "paytm":
+        return parse_paytm(raw_text)
+    elif parser == "phonepe":
+        return parse_phonepe(raw_text)
 
-    elif parser == "gpay":
-        return parse_gpay_pdf(file)
-
-    else:
-        return []
+    return []
